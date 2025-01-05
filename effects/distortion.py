ca8eaab60579d86b3d9a41fba720af1599d9a738
Gmaj7add9 <mmar@student.agh.edu.pl>
2025-01-05 18:25:58 +0100

    Update distortion.py
    już powinien dzialac ez

--- effects/distortion.py
+++ effects/distortion.py
@@ -1,1 +1,10 @@
+from librosa import *
+import numpy as np
+import soundfile as sf
+import matplotlib.pyplot as plt
 
+def apply_Distortion(y, threshold): #y = audio data
+    threshold=abs(threshold)
+    y_distorted=np.clip(y,-threshold, threshold)
+    y_distorted=y_distorted/np.max(np.abs(y_distorted))
+    return y_distorted
