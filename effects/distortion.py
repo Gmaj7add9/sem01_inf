22ec13ebfb1172b4fd1b90c4fba0056628352f2c
Gmaj7add9 <mmar@student.agh.edu.pl>
2025-01-26 11:50:01 +0100

    Update distortion.py
    Distortion bez librosy tylko z pydbuem

--- effects/distortion.py
+++ effects/distortion.py
@@ -1,10 +1,17 @@
-from librosa import *
+from pydub import AudioSegment
 import numpy as np
-import soundfile as sf
-import matplotlib.pyplot as plt
 
-def apply_Distortion(y, threshold): #y = audio data
-    threshold=abs(threshold)
-    y_distorted=np.clip(y,-threshold, threshold)
-    y_distorted=y_distorted/np.max(np.abs(y_distorted))
-    return y_distorted
+def apply_distortion(audio_segment, gain=20, ceiling=32767):
+    samples = np.array(audio_segment.get_array_of_samples())
+    
+    samples = samples * gain
+    
+    samples = np.clip(samples, -ceiling, ceiling)
+    distorted = AudioSegment(
+        samples.astype(np.int16).tobytes(), 
+        frame_rate=audio_segment.frame_rate,
+        sample_width=2, 
+        channels=audio_segment.channels
+    )
+    
+    return distorted
